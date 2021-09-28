bbe5453441a8c6a1a58d4a5636fe38b53fe81123
Luis R <luiscr1104@gmail.com>
2021-09-28 10:18:45 -0600

    Creo funciones y los pruebo desde descuentos.html

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,9 @@
+const cupones= [
+    "cup1",
+    "cup2",
+    "cup3",
+];
+
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento= 100-descuento;
     const precioConDescuento = (precio*porcentajePrecioConDescuento)/100; 
@@ -9,10 +15,23 @@ function onClickButtonPriceDiscount() {
     const inputPrice= document.getElementById("InputPrice");
     const priceValue= inputPrice.value;
 
-    const inputDiscount= document.getElementById("InputDiscount");
-    const discountValue= inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento= calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+    switch (cuponValue) {
+        case cupones[0]:
+            descuento=15;
+            break;
+        case cupones[1]:
+            descuento=30;
+            break;
+        case cupones[2]:
+            descuento=25;
+            break;                           
+    }
+
+    const precioConDescuento= calcularPrecioConDescuento(priceValue, descuento);
 
     const ResultP= document.getElementById("ResultP");
     ResultP.innerText= "El precio con descuento es: "+precioConDescuento+"$";
